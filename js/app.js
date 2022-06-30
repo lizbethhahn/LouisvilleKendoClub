c44500d9ae18fc433455b23bce419cdf29145280
kendoka69 <kendoka69@mac.com>
2022-06-30 13:50:56 -0400

    yet another set of updated videos added

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 var videos = [
-    "https://youtu.be/A4QedTRufIE?t=174", 
-    "https://youtu.be/A4QedTRufIE?t=209", 
-    "https://youtu.be/A4QedTRufIE?t=240",
-    "https://youtu.be/A4QedTRufIE?t=288",
-    "https://youtu.be/A4QedTRufIE?t=326",
-    "https://youtu.be/A4QedTRufIE?t=366",
-    "https://youtu.be/A4QedTRufIE?t=407",
-    "https://youtu.be/A4QedTRufIE?t=518",
-    "https://youtu.be/A4QedTRufIE?t=552",
-    "https://youtu.be/A4QedTRufIE?t=591"
+    "https://youtu.be/U9nNymMSixs?t=186",
+    "https://youtu.be/U9nNymMSixs?t=214",
+    "https://youtu.be/U9nNymMSixs?t=238",
+    "https://youtu.be/U9nNymMSixs?t=273",
+    "https://youtu.be/U9nNymMSixs?t=304",
+    "https://youtu.be/U9nNymMSixs?t=333",
+    "https://youtu.be/U9nNymMSixs?t=364",
+    "https://youtu.be/U9nNymMSixs?t=458",
+    "https://youtu.be/U9nNymMSixs?t=487",
+    "https://youtu.be/U9nNymMSixs?t=515"
 ];
 
 
 
 function getVideo() {
-    var user_input = document.getElementById('myInput').value; //gets value of a text field
+    var user_input = document.getElementById( 'myInput' ).value; //gets value of a text field
 
-    if (user_input < 1 || user_input > videos.length) { //if input is less than 1 or more than 10, alert!
-        alert("Please choose a number 1-10");
+    if ( user_input < 1 || user_input > videos.length ) { //if input is less than 1 or more than 10, alert!
+        alert( "Please choose a number 1-10" );
     } else {
         var index = user_input - 1; //adjusts user input to account for array index system
         var chosen_vid = videos[index]; //accesses video in above array
-        window.open(chosen_vid); //opens video in a separate browers window
+        window.open( chosen_vid ); //opens video in a separate browers window
     }
 }
